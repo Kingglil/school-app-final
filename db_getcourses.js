c65498286e3c85c80e2591e57279c79e85c77c17
Kingglil <mpatrashkov2002@outlook.com>
2018-07-06 10:27:27 +0300

    fix

--- db_getcourses.js
+++ db_getcourses.js
@@ -3,12 +3,30 @@ var util = require('util');
 
 module.exports = async function(con) {
     let sql = "SELECT * FROM courses";
-    let res;
+    let teacherSQL = "SELECT ID, FullName FROM teachers";
 
     con.query = util.promisify(con.query);
 
-    res = await con.query(sql);
+    let courses = await con.query(sql);
     console.log("LOG: User fetched courses.");
+    let teachers = await con.query(teacherSQL);
 
-    return res;
+    let result = [];
+    let flag = false;
+    for(let x = 0; x < courses.length; x++) {
+        for(let y = 0; y < teachers.length; y++) {
+            if(courses[x].TeacherID == teachers[y].ID) {
+                result[x] = courses[x];
+                result[x].TeacherName = teachers[y].FullName;
+                flag = true;
+                break;
+            }
+        }
+        if(!flag) {
+            result[x] = courses[x];
+            result[x].TeacherName = "Unknown";
+        }
+    }
+
+    return result;
 }
--- db_register.js
+++ db_register.js
@@ -1,13 +1,31 @@
 var mysql = require('mysql');
 var util = require('util');
 
+const SECRET_KEY = "123456";
+
 module.exports = async function(con, loginInfo) {
     con.query = util.promisify(con.query);
 
-    let getSQL = "SELECT * FROM teachers";
-    let addSQL = `INSERT INTO teachers (Username, Password, FullName, Subjects) VALUES ('${loginInfo.username}', ` +
-    + `'Pass', 'FN', 'Nemski;Psihologiq')`;
+    if(loginInfo.secretKey != SECRET_KEY) {
+        console.log("WARNING: Somebody tried to registered but failed.");
+        console.log("REASON: The SecretKey did not match.")
+        return { code: 1 };        
+    }
 
+    let getSQL = `SELECT * FROM teachers WHERE Username = '${loginInfo.username}'`;
     let result = await con.query(getSQL);
-    return result;
+    if(result.length > 0) {
+        console.log("WARNING: Somebody tried to registered but failed.");
+        console.log("REASON: A teacher with that username already exists.");
+        return { code: 2 };
+    }
+
+    let addSQL = `INSERT INTO teachers (Username, Password, FullName, Subjects) VALUES ('${loginInfo.username}', ` +
+                 `'${loginInfo.password}', '${loginInfo.fullname}', '${loginInfo.subjects}')`;
+
+    
+
+    await con.query(addSQL);
+    console.log("LOG: A new teacher has just been created.");
+    return { code: 0 }; 
 }
